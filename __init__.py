00288c87df35a4dcc1ada9cb798077b3659233d8
Thorben Bochenek <thorbenb@opera.com>
2013-10-16 13:34:13 +0200

    initial implementation

---
+++ __init__.py
@@ -0,0 +1,134 @@
+from __future__ import print_function
+import sys
+import re
+from css_sort.__version__ import __version__
+
+#
+# The MIT License (MIT)
+
+# Copyright (c) 2013 Einar Lielmanis and contributors.
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+def usage(stream=sys.stdout):
+
+    print("css_sort.py@" + __version__ + """
+
+CSS Sort (https://github.com/bthorben/css_sort)
+
+""", file=stream)
+    if stream == sys.stderr:
+        return 1
+    else:
+        return 0
+
+WHITE_RE = re.compile("^\s+$")
+WORD_RE = re.compile("[\w$\-_]")
+
+
+def sort(source):
+    s = Sorter(source)
+    return s.sort()
+
+
+def sort_file(file_name):
+    if file_name == '-':  # stdin
+        stream = sys.stdin
+    else:
+        stream = open(file_name)
+    content = ''.join(stream.readlines())
+    s = Sorter(content)
+    return s.sort()
+
+
+class Sorter:
+
+    def __init__(self, source):
+        self.source = source
+
+    def sort(self):
+        result = self.source
+        regions = self.getRules()
+        for r in regions:
+            rule = self.source[r[0]+1:r[1]]
+            # print("o (" + str(r) + ":\n" + rule)
+            sortedRule = self.sortRule(rule)
+            # print("s:\n" + sortedRule)
+            result = result[:r[0]+1] + sortedRule + result[r[1]:]
+
+        return result
+
+    def sortRule(self, rule):
+
+        def getName(s):
+            ss = s.split(":")
+            name = ss[0].strip()
+            return re.sub("-o-|-moz-|-webkit-|-ms-", "", name)
+
+        afterLast = None
+        properties = {}
+        props = rule.split(";")
+        for p in props:
+            n = getName(p)
+            if n is not "":
+                properties[n] = p
+            else:
+                afterLast = ";" + p
+        pieces = [properties[n] for n in sorted(properties)]
+        return ";".join(pieces) + (afterLast if afterLast else "")
+
+    def getRules(self):
+
+        def skipTo(pos, string):
+            while pos <= len(self.source) - len(string):
+                pos = pos + 1
+                if self.source[pos:pos + len(string)] == string:
+                    return pos + 1
+            return pos
+
+        regions = []
+        pos = 0
+        rstart = -1
+
+        while pos < len(self.source):
+            ch = self.source[pos]
+            peek = self.source[min(pos + 1, len(self.source) - 1)]
+
+            if ch == "{":
+                rstart = pos
+                pos = pos + 1
+            elif ch == "}":
+                if rstart >= 0:
+                    regions.append((rstart, pos))
+                    rstart = -1
+                pos = pos + 1
+            elif ch == "/" and peek == "*":
+                pos = skipTo(pos, "*/")
+            elif ch == "\"":
+                # TODO: this doesn't take escaped quotes into account
+                pos = skipTo(pos, "\"")
+            elif ch == "'":
+                pos = skipTo(pos, "'")
+            else:
+                pos = pos + 1
+
+        return regions
---
+++ tests/test.py
@@ -0,0 +1,45 @@
+import unittest
+import css_sort
+
+
+class CSSSortTest(unittest.TestCase):
+
+    def testBasics(self):
+        t = self.decodesto
+        t("", "")
+        t("{a:0;b:0}", "{a:0;b:0}")
+        t("{b:0;a:0}", "{a:0;b:0}")
+        t("{a:0;b:0;}", "{a:0;b:0;}")
+        t("{b:0;a:0;}", "{a:0;b:0;}")
+        t("{b:0;a:0}{b:0;a:0}", "{a:0;b:0}{a:0;b:0}")
+
+    def testString(self):
+        t = self.decodesto
+        t('{b: url("test");}{b:0;a:0}', '{b: url("test");}{a:0;b:0}')
+        t('{b: url(\'test\');}{b:0;a:0}', '{b: url(\'test\');}{a:0;b:0}')
+
+    def testWhitespace(self):
+        t = self.decodesto
+        t("{b:0; a:0\n0;}", "{ a:0\n0;b:0;}")
+        t("{b:0; a:0}\n{b:0; a:0}", "{ a:0;b:0}\n{ a:0;b:0}")
+        t("{b:0;\na:0;\n}\n\n{b:0; a:0}", "{\na:0;b:0;\n}\n\n{ a:0;b:0}")
+
+    def testMediaQuery(self):
+        t = self.decodesto
+        t("@media print{{b:0;a:0}}\n{b:0;a:0}",
+          "@media print{{a:0;b:0}}\n{a:0;b:0}")
+        t("@media print{{b:0;a:0\n0}}\n{b:0;a:0\n0}",
+          "@media print{{a:0\n0;b:0}}\n{a:0\n0;b:0}")
+
+    def testPrefix(self):
+        t = self.decodesto
+        t("{b:0;-o-a:0}", "{-o-a:0;b:0}")
+        t("{b:0;-moz-a:0}", "{-moz-a:0;b:0}")
+        t("{b:0;-webkit-a:0}", "{-webkit-a:0;b:0}")
+        t("{b:0;-ms-a:0}", "{-ms-a:0;b:0}")
+
+    def decodesto(self, input, expectation=None):
+        self.assertEqual(css_sort.sort(input), expectation or input)
+
+if __name__ == '__main__':
+    unittest.main()
